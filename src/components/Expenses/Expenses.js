9a1c2cc4fc256f16a37962f026dbca415043d88a
Zilin Xu <zilinxu666@gmail.com>
2023-05-14 20:34:18 -0400

    Using Stateful Lists

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,26 +16,15 @@ export default function Expenses(props) {
 
       <Card className="expenses">
         <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear}/>
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        ></ExpenseItem>
+
+          {props.items.map((expenses) => (
+              <ExpenseItem
+                  key={expenses.id}
+                  title={expenses.title}
+                  amount={expenses.amount}
+                  date={expenses.date}
+              />
+          ))}
       </Card>
         </div>
     );
